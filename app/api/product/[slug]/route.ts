60463caa970081921d93f1c77920f98846bf812f
iodzaMonk <iodzamonkmode@gmail.com>
2025-10-17 16:23:15 -0400

    [Upd] Added request for GET

--- app/api/product/[slug]/route.ts
+++ app/api/product/[slug]/route.ts
@@ -3,9 +3,8 @@ import { NextResponse } from "next/server";
 import { Prisma } from "@prisma/client";
 import { createSignedDownloadUrl } from "@/app/lib/storage/s3";
 
-
-
 export async function GET(
+  request: Request,
   { params }: { params: { slug: string } },
 ) {
   const id = await params;
@@ -22,14 +21,15 @@ export async function GET(
       );
     }
     async function getProductWithImage() {
-
       if (product) {
         if (!product.image_location) {
           return product;
         }
 
         try {
-          const imageUrl = await createSignedDownloadUrl(product.image_location);
+          const imageUrl = await createSignedDownloadUrl(
+            product.image_location,
+          );
           return {
             ...product,
             image_url: imageUrl,
@@ -38,7 +38,6 @@ export async function GET(
           console.error("Failed to sign image url", error);
           return product;
         }
-
       }
     }
 
