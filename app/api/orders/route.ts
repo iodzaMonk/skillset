92ce6a172344c594bc9d24c33f8fbad1a4ba1cc1
Mohamed Arezki Brahimi <bramomoh06@gmail.com>
2025-10-29 18:45:20 -0400

    [Init]: added email during order creation

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -1,15 +1,21 @@
 import { headers } from "next/headers";
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
-
+import { Resend } from "resend";
 export async function POST(request: Request) {
   const headersList = await headers();
   const referer = headersList.get("referer") ?? "";
-
+  const resend = new Resend(process.env.EMAIL_API!);
   try {
     const body = await request.json();
     const { description, prof_id, productId, userId } = body;
-
+    const professional = await prisma.users.findUnique({
+      where: { id: prof_id },
+    });
+    const clientName = await prisma.users.findUnique({
+      where: { id: userId },
+      select: { name: true },
+    });
     const order = await prisma.commands.create({
       data: {
         client_id: userId,
@@ -19,7 +25,13 @@ export async function POST(request: Request) {
         date: new Date(),
       },
     });
-
+    const mailContent = `Hello, you have a new order with the following details:\n\nDescription: ${description}\nProduct ID: ${productId}\nFrom client: ${clientName?.name || "Unknown"}`;
+    resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: professional?.email || "bramomoh06@gmail.com",
+      subject: "New Order Received",
+      html: `<p>${mailContent}</p>`,
+    });
     return NextResponse.json(
       { id: order.id, message: "Order processed successfully" },
       { status: 201, headers: { "x-referer": referer } },
